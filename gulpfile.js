80f385e4b4375de0025ced900e6681d1ac14b582
faliherizo <faliherizo@gmail.com>
2016-12-23 11:47:12 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var typescript = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+
+var tsProject = typescript.createProject('tsconfig.json');
+gulp.task('default', function() {
+    // place code for your default task here
+});
